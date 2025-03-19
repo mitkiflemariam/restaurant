78ddc9920836c12c364c2e6ef1506d967ed149fc
Sb-CCL <sbcolorcodedlabs@gmail.com>
2025-03-19 17:24:19 -0400

    Resolved updated the  RequestReset.jsx

--- client/src/components/main/RequestReset.jsx
+++ client/src/components/main/RequestReset.jsx
@@ -1,16 +1,9 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
-import { Button } from "../ui/button";
-import { requestPasswordReset } from "@/lib/utils"
-import { Input } from "../ui/input";
-=======
-
 import { requestPasswordReset } from "@/lib/utils";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
->>>>>>> ui
 
 function RequestReset() {
   const [email, setEmail] = useState("");
@@ -27,43 +20,6 @@ function RequestReset() {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="flex items-center text-black justify-center min-h-screen ">
-        <div className="p-6 shadow-xl rounded-lg border bg-card text-card-foreground overflow-hidden flex flex-col h-full w-96">
-          <h2 className="text-2xl font-bold text-center mb-4">Request Password Reset</h2>
-          {message && <p>{message}</p>}
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                
-                <Input
-                  
-                  type="email"
-                
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Enter your email"
-                  required
-                 />
-              </div>
-              
-              <Button type="submit" className="w-full" >
-              Send Reset Link
-              </Button>
-            </div>
-            
-          </form>
-         
-        </div>
-      </div>
-    // <div>
-    //   <h2>Request Password Reset</h2>
-    //   {message && <p>{message}</p>}
-    //   <form onSubmit={handleSubmit}>
-    //     <input type="email" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} required />
-    //     <button type="submit">Send Reset Link</button>
-    //   </form>
-    // </div>
-=======
     <div className="flex items-center text-black justify-center min-h-screen">
       <div className="p-6 shadow-xl rounded-lg border bg-card text-card-foreground overflow-hidden flex flex-col h-full w-96">
         <h2 className="text-2xl font-bold text-center mb-4">Reset Password</h2>
@@ -102,7 +58,6 @@ function RequestReset() {
         </form>
       </div>
     </div>
->>>>>>> ui
   );
 }
 
